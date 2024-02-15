f3f0403172169bdd5f12c451e0ff9b378568534f
Latex Kitty Cass <70722603+Lat3xKitty@users.noreply.github.com>
2024-02-15 21:25:34 +0000

    Add files via upload

--- js/constants.js
+++ js/constants.js
@@ -2,8 +2,8 @@
     booksURL can be on a different domain/subdomain if you prefer.
     The books are stored in a library. The library name can be whatever you want (here it's books)
 */
-export function homeURL() {return           "https://late-stars-tease.loca.lt/"}
-export function readerURL() {return         "https://late-stars-tease.loca.lt/read.html"}
-export function booksURL() {return          "https://late-stars-tease.loca.lt/library/"}
+export function homeURL() {return           "https://lat3xkitty.com/darkflame/"}
+export function readerURL() {return         "https://lat3xkitty.com/darkflame/read.html"}
+export function booksURL() {return          "https://lat3xkitty.com/darkflame/library/"}
 export function websiteName() {return       "DarkFlame Universe"}
 export function defaultLanguage() {return   "en"}
